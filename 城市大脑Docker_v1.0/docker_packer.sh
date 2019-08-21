adc39d6da179b043d2c33c72bf1acab06fa7bd50
whosyourmami <724517470@qq.com>
2019-08-21 11:58:30 +0800

    update

---
+++ 城市大脑Docker_v1.0/docker_packer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#命令执行格式
+#./docker_packer.sh tonglu_webapp:1.1.0
+name=$1
+cd /home/docker
+docker build --rm --tag "$name" .
+#docker run -d -p 81:7777 "$name"
+docker save -o images/$(date +"%Y%m%d%H%M%S").tar "$name"
