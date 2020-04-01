1d6cdfd7cd665b6e242259bceaa5c2a8f1978294
Administrator <pjl@hleast.com>
2020-04-01 09:56:46 +0800

    rename

--- 亲清在线/前端docker_packer.sh
+++ 亲清在线/前端docker_packer.sh
@@ -1,22 +1,30 @@
 #变量
 PRE=registry.citybrain.local/a6013-pre/
-SOURCE_IMAGE=py_330124_001_gov_backend
+SOURCE_IMAGE=py_330124_001_gov_pc
 TAG=1.0.$BUILD_ID
 
-
-cp /home/dockerfile_backend_qqzx $WORKSPACE/target/dockerfile
-cd $WORKSPACE/target
+node -v
+npm -v
+cnpm -v
+cnpm i
+#执行构建命令
+npm run build
+cp /home/dockerfile_v1.0 $WORKSPACE/dockerfile
+sed -i s/dist/xhWater/ $WORKSPACE/dockerfile
+cp /home/nginx.conf_v1.0 $WORKSPACE/nginx.conf
+cp /home/default.conf_v1.0 $WORKSPACE/default.conf
+cd $WORKSPACE
 sudo docker version
 #打包docker镜像
-#sudo docker build --rm --tag py_330124_001_gov_backend:1.0.$BUILD_ID .
+#sudo docker build --rm --tag py_330124_001_gov_pc:1.0.$BUILD_ID .
 sudo docker build --rm --tag ${PRE}${SOURCE_IMAGE}:${TAG} .
 #另存为镜像至工作区
-#sudo docker save -o ./py_330124_001_gov_backend_1.0.$BUILD_ID.tar py_330124_001_gov_backend:1.0.$BUILD_ID
+#sudo docker save -o ./py_330124_001_gov_pc_1.0.$BUILD_ID.tar py_330124_001_gov_pc:0.0.$BUILD_ID
 sudo docker save -o ./${SOURCE_IMAGE}_${TAG}.tar ${PRE}${SOURCE_IMAGE}:${TAG}
 #更改文件属组
 sudo chown jenkins:jenkins ./${SOURCE_IMAGE}_${TAG}.tar
 #删除镜像
-#sudo docker rmi -f $(docker images | grep registry.citybrain.local/a6013-pre/py_330124_001_gov_backend | awk '{print $3}')
+#sudo docker rmi $(docker images | grep py_330124_001_gov_pc | awk '{print $3}')
 sudo docker rmi $(docker images | grep ${PRE}${SOURCE_IMAGE}:${TAG})
 #删除无用镜像
 #echo y| sudo docker image prune
