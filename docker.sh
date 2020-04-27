aebd37fbb8d47fc8725ff636a05073b6316c892a
Administrator <pjl@hleast.com>
2020-04-27 17:11:57 +0800

    fixed

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 curl -skSL https://mirror.azure.cn/repo/install-docker-ce.sh | sh -s -- --mirror Aliyun
 
-cat /etc/docker/daemon.json <<- 'EOF'
+cat > /etc/docker/daemon.json <<- 'EOF'
 {
   "registry-mirrors": ["http://hub-mirror.c.163.com"]
 }
